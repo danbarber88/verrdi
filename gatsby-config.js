7d042611b2cf1bee8757f1545d90d1d2b71ef760
Dan Barber <d.j.barber88@gmail.com>
2019-10-01 18:54:37 +0100

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: `Verrdi Training & Recruitment`,
+    description: "PLACEHOLDER DESCRIPTION",
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#014124`,
+        theme_color: `#014124`,
+        display: `minimal-ui`,
+        icon: ``, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-plugin-robots-txt",
+      options: {
+        host: "",
+        sitemap: "",
+        policy: [{ userAgent: "*", disallow: "/" }],
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
