b68f77521aebc0e098aa8db62c08dbc0e7c1fe05
Dan Barber <d.j.barber88@gmail.com>
2019-10-03 11:03:58 +0100

    Add theme colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,24 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from "react"
+import PropTypes from "prop-types"
+import { ThemeProvider } from "styled-components"
+
+const theme = {
+  lightGreen: "#006738",
+  green: "#014124",
+  darkGreen: "002112",
+  yellow: "F9AD35",
+}
 
 const Layout = ({ children }) => {
-  return <main>{children}</main>;
-};
+  return (
+    <ThemeProvider theme={theme}>
+      <main>{children}</main>
+    </ThemeProvider>
+  )
+}
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired
-};
+  children: PropTypes.node.isRequired,
+}
 
-export default Layout;
+export default Layout
