b4830b89540ed1bd9051f08fbe64e8301a5082c4
Dan Barber <d.j.barber88@gmail.com>
2019-11-11 17:30:14 +0000

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,42 @@
 import React from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import { device } from "../utils/device"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Button from "../components/button"
 
-const NotFoundPage = () => (
+const Container = styled.div`
+  margin: 150px 20px;
+  color: ${props => props.theme.green};
+  text-align: center;
+`
+
+const Header = styled.h1`
+  font-size: 4rem;
+  @media ${device.tablet} {
+    font-size: 2rem;
+  }
+`
+
+const Text = styled.p`
+  font-size: 2rem;
+  @media ${device.tablet} {
+    font-size: 1rem;
+  }
+`
+
+const NotFoundPage = props => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Header>NOT FOUND</Header>
+      <Text>You just hit a page that doesn't exist.</Text>
+      <Link to="/">
+        <Button>Go Home</Button>
+      </Link>
+    </Container>
   </Layout>
 )
-
 export default NotFoundPage
