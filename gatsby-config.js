f679edc12620fd5137671d40179e5b99ccbffe44
Dan Barber <d.j.barber88@gmail.com>
2019-12-08 15:28:06 +0000

    clean out GA options

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,16 +67,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: `${process.env.GATSBY_GOOGLE_ANALYTICS_ID}`,
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
