806d01288a0c32a9c73cc108ae84be147cb462f9
Dan Barber <d.j.barber88@gmail.com>
2020-01-07 19:37:24 +0000

    dont use vh, doesnt work on mobiles very well.

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,8 +126,14 @@ const TempContainer = styled.div`
   justify-content: center;
   align-items: center;
   width: 100%;
-  height: 100vh;
+  /* height: 100%; */
   background-color: ${props => props.theme.green};
+
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 `
 
 const StyledTempLogo = styled(TempLogo)`
