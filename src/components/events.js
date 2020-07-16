699a79349be2db1aabcf0eae39054fc246659831
Dan Barber <d.j.barber88@gmail.com>
2020-07-16 12:10:37 +0100

    handle api errors

--- src/components/events.js
+++ src/components/events.js
@@ -14,6 +14,21 @@ const Spinner = styled(FontAwesomeIcon)`
   color: ${props => props.theme.green};
   font-size: 70px;
   margin: 100px 0;
+  width: auto;
+`
+
+const Error = styled.div`
+  background-color: #791b1b;
+  color: #fff;
+  padding: 20px 10px;
+  text-align: center;
+  margin: 40px 0;
+  border-radius: 10px;
+  box-shadow: 1px 2px 4px rgba(0,0,0,0.12), 1px 3px 4px rgba(0,0,0,0.24), 0px -1px 4px rgba(0,0,0,0.25);
+
+  a {
+    color: ${props => props.theme.yellow}
+  }
 `
 
 class Events extends Component {
@@ -22,6 +37,7 @@ class Events extends Component {
 
     this.state = {
       loading: true,
+      error: false
     }
   }
 
@@ -30,7 +46,12 @@ class Events extends Component {
       `https://www.eventbriteapi.com/v3/users/me/events/?expand=ticket_availability,venue,ticket_classes&token=${process.env.GATSBY_EVENTBRITE_API_KEY}`
     )
     const data = await response.json()
-    this.setState({ loading: false, data: data.events })
+
+    if(data.status_code === 200) {
+      this.setState({ loading: false, data: data.events })
+    } else {
+      this.setState({ loading: false, error: true })
+    }
   }
 
   componentDidMount() {
@@ -38,6 +59,14 @@ class Events extends Component {
   }
 
   render() {
+    if (this.state.error) {
+      return <Error>
+          <h3>Oops! Something went wrong.</h3>
+          <p>Send an error report to help us improve your experience.</p>
+          <a  href="mailto:whoops@verrdi.co.uk">whoops@verrdi.co.uk</a>
+        </Error>
+    }
+
     return (
       <div>
         {this.state.loading ? (
